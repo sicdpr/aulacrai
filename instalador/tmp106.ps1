b85aab5533967fc80301f52cbd7efed27a0ee3d5
sicdpr <sicdpr@unileon.es>
2026-03-04 10:10:45 +0100

    Update tmp106.ps1

--- instalador/tmp106.ps1
+++ instalador/tmp106.ps1
@@ -2,15 +2,16 @@ Write-Host "Iniciando instalación combinada (AutoFirma + Google Earth Pro)..." 
 
 # 1. Instalación de AutoFirma
 Write-Host "1/2 Instalando AutoFirma..." -ForegroundColor Cyan
-winget install --id SpanishGovernment.Autofirma --silent --accept-package-agreements --accept-source-agreements
+winget install --id SpanishGovernment.Autofirma --silent --accept-package-agreements --accept-source-agreements --scope machine
 
 # 2. Instalación de Google Earth Pro
 Write-Host "2/2 Instalando Google Earth Pro..." -ForegroundColor Cyan
-winget install --id Google.EarthPro --silent --accept-package-agreements --accept-source-agreements
+winget install --id Google.EarthPro --silent --accept-package-agreements --accept-source-agreements --scope machine
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Ambas aplicaciones se han instalado correctamente." -ForegroundColor Green
 } else {
     Write-Host "El proceso finalizó, pero algunas instalaciones podrían haber fallado." -ForegroundColor Red
 }
+
 
